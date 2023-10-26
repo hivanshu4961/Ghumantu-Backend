c533bfc750fc22547e75abfbc1e4a74be822d826
anirudh loona <hivanshutyagi4961@gmail.com>
2023-10-26 12:54:30 +0530

    first commit

---
+++ src/main/java/com/example/ghumantu/GhumantuApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghumantu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhumantuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhumantuApplication.class, args);
+	}
+
+}
